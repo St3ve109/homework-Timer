434ee68d134517319b455b8991baf12fdfb0edae
St3ve109 <stephenjackson1214@gmail.com>
2022-11-12 13:48:38 -0500

    todo list start

--- Index.js
+++ Index.js
@@ -91,28 +91,10 @@ function runTime() {
     }   
 }
 //====================================To_do list==================================================
-document.querySelector('#add').onclick = function(){
-    if(document.querySelector('#To_do input').value.length == 0){
-        alert("Please Enter a Task")
-    }
 
-    else{
-        document.querySelector('#tasks').innerHTML += `
-            <div class="task">
-                <span id="taskname">
-                    ${document.querySelector('#To_do input').value}
-                </span>
-                <button class="delete">
-                    <i class="far fa-trash-alt"></i>
-                </button>
-            </div>
-        `;
-
-        var current_tasks = document.querySelectorAll(".delete");
-        for(var i=0; i<current_tasks.length; i++){
-            current_tasks[i].onclick = function(){
-                this.parentNode.remove();
-            }
-        }
+function add(){
+    var boxImput = document.getElementById("box").value
+    if(boxImput !=""){
+        console.log(boxImput)
     }
 }
